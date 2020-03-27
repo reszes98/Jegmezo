ea807b947f632eb3cf6746ea8fe6775b3ca3cc21
Henya14 <henrikeloe@gmail.com>
2020-03-27 13:49:01 +0100

    Irany enum

---
+++ Jegmezo/Irany.java
@@ -0,0 +1,13 @@
+package Jegmezo;
+
+/**
+ * @author Henrik
+ * A játékban a Jégtáblák lehetséges szomszédos irányait képviseli. (Jobb(0), Bal(1), Fel(2),
+Le(3))
+ */
+public enum Irany {
+Jobb,
+Bal,
+Fel,
+Le
+}
