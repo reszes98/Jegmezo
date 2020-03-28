d1317106a615a9f89692a305387f62e10ef0d922
ekkd25 <jonasemese21@gmail.com>
2020-03-28 14:13:08 +0100

    Menupontjaim, kivéve a hóvihart

--- Jegmezo/Menu.java
+++ Jegmezo/Menu.java
@@ -19,23 +19,51 @@ public class Menu {
 			List<Targy> targyak=new ArrayList<Targy>();
 			Sarkkutato j=new Sarkkutato(jm,jt,0,5,5,targyak,Irany.Jobb);
 			Aso a=new Aso();
+			Kotel k = new Kotel();
+			Buvarruha b = new Buvarruha();
+			Elelem e = new Elelem();
+			Jelzopisztoly jp = new Jelzopisztoly();
 			
 			
 		System.out.print("Mit szeretne? (nyomja meg a megfelelõ menüpontot)\n 1 - Ásás\n 2 - Ásó felvesz\n 3 - Ásó használ");
-		answer =  in.nextInt();
 		
-		switch(answer) {
-			case 1:
-				j.asas();
-		    	break;
-			case 2:
-				
-		    	break;
-			case 3:
-				
-		    	break;
-			default:
-				System.out.println("Válasszon a megadott menüpontok közül");
-		}
+		do {
+			answer =  in.nextInt();
+			
+			switch(answer) {
+				case 1:
+					j.asas();
+			    	break;
+				case 2:
+					a.felvesz(j);
+			    	break;
+				case 3:
+					
+			    	break;
+				case 4:
+					b.felvesz(j);
+			    	break;
+			    	
+				case 6:
+					e.felvesz(j);
+			    	break;
+			    	
+				case 8:
+					
+			    	break;	
+				case 10:
+					jp.felvesz(j);
+			    	break;
+			    	
+				case 12:
+					k.felvesz(j);
+			    	break;
+				case 13:
+					k.hasznal(j);
+			    	break;
+				default:
+					System.out.println("Válasszon a megadott menüpontok közül");
+			}
+		}while(answer != 66);
 	}
 }
