38226a6c96b66c797403a3cdf2ef254d49babc5e
FAFS4X <rozmaringflora@hotmail.com>
2020-05-09 13:38:07 +0200

    irany

--- Jegmezo/DrawKotel.java
+++ Jegmezo/DrawKotel.java
@@ -11,6 +11,11 @@ package Jegmezo;
 //
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class DrawKotel extends Drawable {
 	private Image kotelImage;
