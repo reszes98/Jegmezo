7de88566ee882ba9f00555876451c3e6fb2aa450
FAFS4X <rozmaringflora@hotmail.com>
2020-05-09 13:22:38 +0200

    draw sator, sarkk., tor.aso

--- Jegmezo/DrawSarkkutato.java
+++ Jegmezo/DrawSarkkutato.java
@@ -11,12 +11,45 @@ package Jegmezo;
 //
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class DrawSarkkutato extends Drawable {
 	private Image sarkkutatoImage;
+	public DrawSarkkutato() {
+		try {
+			sarkkutatoImage=ImageIO.read(new File("./image/explorer.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public void draw(Graphics g) {
+		
+		g.drawImage(sarkkutatoImage, getPositionX(), getPositionY(), null);
 	}
 	
-	public void setPosition(int px, int py) {
+	public void setPosition(int px, int py) 
+	{
+		super.setPosition(px, py);
+	}
+	public void setPositionX(int px) 
+	{
+		super.setPositionX(px);
+	}
+	public void setPositionY(int py) 
+	{
+		super.setPositionY(py);
+	}
+	public int getPositionX() 
+	{
+		return super.getPositionX();
+	}
+	public int getPositionY() 
+	{
+		return super.getPositionY();
 	}
 }
